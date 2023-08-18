7367db580e8924d5132f40a49fbc3a7b5e0c81df
Oghenetega Udi <oghenetega.udi@Interswitchng.com>
2023-08-18 15:16:26 +0100

    Add new UpperLowerCase class

--- src/main/java/com/interswitch/test/App.java
+++ src/main/java/com/interswitch/test/App.java
@@ -8,16 +8,22 @@ import com.interswitch.test.service.CalculatorService;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         CalculatorService calculator = new CalculatorImpl();
-        System.out.println(calculator.addition(4,5));
-        System.out.println(calculator.difference(4,5));
-        System.out.println(calculator.divide(4,5));
-        System.out.println(calculator.multiply(4,5));
+        System.out.println(calculator.addition(4, 5));
+        System.out.println(calculator.difference(4, 5));
+        System.out.println(calculator.divide(4, 5));
+        System.out.println(calculator.multiply(4, 5));
 
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+
+        UpperLowerCase upercls = new UpperLowerCase();
+        System.out.printf("%s to %s","Hello World", upercls.wordToUpper("Hello World!"));
+        System.out.println();
+        System.out.printf("%s to %s","Hello World", upercls.wordToLower("Hello World!"));
+
     }
 }
+
+
